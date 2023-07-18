ac13ad39b7d7694ddc8f2e148a9a442e87dad4da
Rostichelli <leskalves.douglas@gmail.com>
2023-07-18 16:15:59 -0300

    H2 and jpa add

--- src/main/java/com/example/springboot/entities/User.java
+++ src/main/java/com/example/springboot/entities/User.java
@@ -1,10 +1,16 @@
 package com.example.springboot.entities;
 
+import jakarta.persistence.*;
+
 import java.io.Serializable;
 import java.util.Objects;
 
+@Entity
+@Table(name = "tb_user") 
 public class User implements Serializable {
     private static final Integer serialVersionUID = 1;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String name;
     private String email;
